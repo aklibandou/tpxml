64b795a17d1f7ab1f7a240721cbadb90d2e47e94
aklibandou <78756360+aklibandou@users.noreply.github.com>
2021-02-10 16:54:17 +0100

    Update meteo.js

--- meteo.js
+++ meteo.js
@@ -1,27 +1,21 @@
-		
-			
-		function showCity() {
-			let villeCourante = document.getElementById("city-select").value;
-			if(villeCourante!=""){
-				document.getElementById("selection").innerHTML= "Vous avez séléctionné "+villeCourante;
+
+function showCity() {
+	let villeCourante = document.getElementById("city-select").value;
+	if(villeCourante!=""){
+			document.getElementById("selection").innerHTML= "Vous avez séléctionné "+villeCourante;
+	}else{
+		document.getElementById("selection").innerHTML= " ";
+	}	
+	
+	let cities = document.getElementsByClassName("city");
+	for( let i=0;i<cities.length;i++){
+		if(cities[i].id==villeCourante){	
+			cities[i].style.display ="block";	
 			}else{
-				document.getElementById("selection").innerHTML= " ";
+				cities[i].style.display ="";
 			}
-				
-						
-				
-			let c = document.getElementsByTagName("span");
-				
-			
-			for( let i=0;i<c.length;i++){
-					
-				if(c[i].id==villeCourante){	
-					c[i].style.display ="block";	
-				}else{
-					c[i].style.display ="";
-				}
-			}
-				
-				
 		}
+				
+				
+	}
 	
